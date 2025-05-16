2178ddea8d7eac7e2b1dece6616017edcbeee4a0
Spadaro Gian Franco <gianfranco.spadaro@edu.esiee.fr>
2025-05-16 17:16:20 +0200

    Update : Fixed VR Again

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -2,70 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR;
 
 public class OpenDoor : MonoBehaviour
 {
     public Animator animator;
 
     private bool isClose = true;
-
     private bool canMove = true;
 
     public TMP_Text opendoor;
-    // Start is called before the first frame update
+
+    private InputDevice device;
+    public XRNode controllerNode = XRNode.RightHand; // Change to LeftHand if needed
+
     void Start()
     {
         opendoor.gameObject.SetActive(false);
+        device = InputDevices.GetDeviceAtXRNode(controllerNode);
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        // Refresh the device if not valid
+        if (!device.isValid)
+        {
+            device = InputDevices.GetDeviceAtXRNode(controllerNode);
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("RealPlayer"))
         {
-            //Debug.Log("player");
             if (this.name == "doorhitbox1")
             {
                 opendoor.gameObject.SetActive(true);
             }
-            if (Input.GetKey(KeyCode.E) && isClose && canMove)
+
+            bool isPressed = false;
+            if (device.TryGetFeatureValue(CommonUsages.triggerButton, out isPressed) && isPressed)
             {
-                Debug.Log("Open");
-                canMove = false;
-                animator.SetTrigger("open");
-                isClose = false;
-                StartCoroutine(WaitDoor());
-                
-            }
-            if (Input.GetKey(KeyCode.E) && !isClose && canMove)
-            {
-                Debug.Log("Close");
-                canMove = false;
-                animator.SetTrigger("close");
-                isClose = true;
-                StartCoroutine(WaitDoor());
-                
-            }
-            if (this.name == "r2text")
-            {
-                Debug.Log("cccccccccccc");
+                if (isClose && canMove)
+                {
+                    Debug.Log("Open");
+                    canMove = false;
+                    animator.SetTrigger("open");
+                    isClose = false;
+                    StartCoroutine(WaitDoor());
+                }
+                else if (!isClose && canMove)
+                {
+                    Debug.Log("Close");
+                    canMove = false;
+                    animator.SetTrigger("close");
+                    isClose = true;
+                    StartCoroutine(WaitDoor());
+                }
+
+                if (this.name == "r2text")
+                {
+                    Debug.Log("cccccccccccc");
+                }
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        opendoor.gameObject.SetActive(false);
+        if (other.gameObject.CompareTag("RealPlayer"))
+        {
+            opendoor.gameObject.SetActive(false);
+        }
     }
 
     private IEnumerator WaitDoor()
     {
-        yield return new WaitForSeconds(1f); // Adjust the delay time as needed
+        yield return new WaitForSeconds(1f);
         canMove = true;
     }
 }
